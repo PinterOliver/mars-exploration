2127e5375575084d9e2d704144993cf7f3203204
Oliver Peter Pinter <125395118+PinterOliver@users.noreply.github.com>
2023-09-18 21:06:17 +0200

    add add method to Coordinate record

--- src/main/java/com/codecool/marsexploration/data/utilities/Coordinate.java
+++ src/main/java/com/codecool/marsexploration/data/utilities/Coordinate.java
@@ -1,4 +1,18 @@
 package com.codecool.marsexploration.data.utilities;
 
+import org.jetbrains.annotations.NotNull;
+
 public record Coordinate(int row, int column) {
+  public static @NotNull Coordinate add(Coordinate coordinate1, Coordinate coordinate2) {
+    coordinate1 = getNotNullCoordinate(coordinate1);
+    coordinate2 = getNotNullCoordinate(coordinate2);
+    return new Coordinate(coordinate1.row + coordinate2.row, coordinate1.column + coordinate2.column);
+  }
+  
+  private static @NotNull Coordinate getNotNullCoordinate(Coordinate coordinate) {
+    if (coordinate == null) {
+      return new Coordinate(0, 0);
+    }
+    return coordinate;
+  }
 }
