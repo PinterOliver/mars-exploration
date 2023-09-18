39ba24012498cb91704f7daf5309ed44f114c09a
Oliver Peter Pinter <125395118+PinterOliver@users.noreply.github.com>
2023-09-18 12:32:39 +0200

    add xml file for code formatting

--- src/main/java/com/codecool/marsexploration/Application.java
+++ src/main/java/com/codecool/marsexploration/Application.java
@@ -3,8 +3,8 @@ package com.codecool.marsexploration;
 import java.util.Random;
 
 public class Application {
-    public static void main(String[] args) {
-        Random random = new Random();
-        System.out.println(random.nextInt(42));
-    }
+  public static void main(String[] args) {
+    Random random = new Random();
+    System.out.println(random.nextInt(42));
+  }
 }
