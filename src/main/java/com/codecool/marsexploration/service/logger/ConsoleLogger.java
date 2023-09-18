69d7a7d8fb71ae63e321737a8ad12a4a61768b2a
Oliver Peter Pinter <125395118+PinterOliver@users.noreply.github.com>
2023-09-18 12:37:39 +0200

    add ConsoleLogger class

---
+++ src/main/java/com/codecool/marsexploration/service/logger/ConsoleLogger.java
@@ -0,0 +1,41 @@
+package com.codecool.marsexploration.service.logger;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+
+public class ConsoleLogger implements Logger {
+  @Override
+  public void logInfo(String message) {
+    logMessage("INFO", message);
+  }
+  
+  @Override
+  public void logInfo(String @NotNull [] messages) {
+    for (int i = 0; i < messages.length; i++) {
+      logInfo(String.format("%d: %s", i + 1, messages[i]));
+    }
+  }
+  
+  @Override
+  public void logInfo(@NotNull Collection<String> collection) {
+    String[] stringArray = collection.toArray(String[]::new);
+    logInfo(stringArray);
+  }
+  
+  @Override
+  public void logError(String message) {
+    logMessage("ERROR", message);
+  }
+  
+  @Override
+  public void displayMessage(String message) {
+    System.out.println(message);
+  }
+  
+  private void logMessage(String type, String message) {
+    LocalDateTime now = LocalDateTime.now();
+    displayMessage(String.format("[%tF %tT] %s: %s", now, now, type, message));
+  }
+}
