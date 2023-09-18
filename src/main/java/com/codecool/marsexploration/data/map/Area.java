bce7adc072b77b85228d9e9503d730726b5fe401
Oliver Peter Pinter <125395118+PinterOliver@users.noreply.github.com>
2023-09-18 21:08:27 +0200

    add Area class

---
+++ src/main/java/com/codecool/marsexploration/data/map/Area.java
@@ -0,0 +1,38 @@
+package com.codecool.marsexploration.data.map;
+
+import com.codecool.marsexploration.data.cell.Cell;
+import com.codecool.marsexploration.data.cell.CellType;
+import com.codecool.marsexploration.data.utilities.Coordinate;
+
+public class Area {
+  protected final int height;
+  protected final int width;
+  protected final Cell[][] cells;
+  
+  public Area(int height, int width) {
+    this.height = height;
+    this.width = width;
+    this.cells = new Cell[this.height][this.width];
+    fillCellsWithEmptyCells();
+  }
+  
+  @Override
+  public String toString() {
+    StringBuilder stringBuilder = new StringBuilder();
+    for (int row = 0; row < height; row++) {
+      for (int column = 0; column < width; column++) {
+        stringBuilder.append(cells[row][column].toString());
+      }
+      stringBuilder.append("\n");
+    }
+    return stringBuilder.toString();
+  }
+  
+  private void fillCellsWithEmptyCells() {
+    for (int row = 0; row < height; row++) {
+      for (int column = 0; column < width; column++) {
+        cells[row][column] = new Cell(new Coordinate(row, column), CellType.EMPTY, this);
+      }
+    }
+  }
+}
