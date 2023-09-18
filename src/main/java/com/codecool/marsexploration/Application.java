5b5ed5fa7b4af8d5759c2ed455abc98715250bab
Oliver Peter Pinter <125395118+PinterOliver@users.noreply.github.com>
2023-09-18 12:49:16 +0200

    refactor main method of Application

--- src/main/java/com/codecool/marsexploration/Application.java
+++ src/main/java/com/codecool/marsexploration/Application.java
@@ -1,10 +1,20 @@
 package com.codecool.marsexploration;
 
+import com.codecool.marsexploration.service.filewriter.EasyFileWriter;
+import com.codecool.marsexploration.service.filewriter.EasyFileWriterImpl;
+import com.codecool.marsexploration.service.logger.ConsoleLogger;
+import com.codecool.marsexploration.service.logger.Logger;
+import com.codecool.marsexploration.service.utilities.Pick;
+import com.codecool.marsexploration.service.utilities.PickImpl;
+
 import java.util.Random;
 
 public class Application {
+  private static final Logger LOGGER = new ConsoleLogger();
+  private static final EasyFileWriter FILE_WRITER = new EasyFileWriterImpl(LOGGER);
+  private static final Random RANDOM = new Random();
+  private static final Pick PICK = new PickImpl(RANDOM);
+  
   public static void main(String[] args) {
-    Random random = new Random();
-    System.out.println(random.nextInt(42));
   }
 }
