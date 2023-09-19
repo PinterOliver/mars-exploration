0dd6b72f0acb9d4ccb2a59d30059138b5a5a7728
Oliver Peter Pinter <125395118+PinterOliver@users.noreply.github.com>
2023-09-19 12:01:38 +0200

    add MapConfigurationValidation interface

---
+++ src/main/java/com/codecool/marsexploration/service/validation/MapConfigurationValidation.java
@@ -0,0 +1,7 @@
+package com.codecool.marsexploration.service.validation;
+
+import com.codecool.marsexploration.data.config.MapConfiguration;
+
+public interface MapConfigurationValidation {
+  boolean isValid(MapConfiguration configuration);
+}
