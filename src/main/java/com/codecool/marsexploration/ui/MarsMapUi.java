bbb8ca726b9dcca6bed8dbd6396dff0aab66bfe2
Oliver Peter Pinter <125395118+PinterOliver@users.noreply.github.com>
2023-09-19 23:33:15 +0200

    add constructor to MarsMapUi class

--- src/main/java/com/codecool/marsexploration/ui/MarsMapUi.java
+++ src/main/java/com/codecool/marsexploration/ui/MarsMapUi.java
@@ -1,4 +1,37 @@
 package com.codecool.marsexploration.ui;
 
+import com.codecool.marsexploration.data.cell.CellType;
+import com.codecool.marsexploration.data.config.MapConfiguration;
+import com.codecool.marsexploration.data.config.MapValidationConfiguration;
+import com.codecool.marsexploration.data.config.RangeConfiguration;
+import com.codecool.marsexploration.data.config.ResourceConfiguration;
+import com.codecool.marsexploration.service.input.Input;
+import com.codecool.marsexploration.service.logger.Logger;
+import com.codecool.marsexploration.service.map.MapManager;
+import com.codecool.marsexploration.service.validation.MapConfigurationValidator;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public class MarsMapUi {
+  private final Logger logger;
+  private final Input input;
+  private final MapManager manager;
+  private final MapConfigurationValidator validator;
+  private final MapValidationConfiguration validationConfiguration;
+  private final String filePathFormat;
+  private int remainingFreeTiles;
+  
+  public MarsMapUi(Logger logger, Input input, MapManager mapManager, MapConfigurationValidator validator,
+          MapValidationConfiguration validationConfiguration, String filePathFormat) {
+    this.logger = logger;
+    this.input = input;
+    this.manager = mapManager;
+    this.validator = validator;
+    this.validationConfiguration = validationConfiguration;
+    this.filePathFormat = filePathFormat;
+  }
 }
