d94402770f47bc4e80f2577d8c879f78f6dc35f5
Oliver Peter Pinter <125395118+PinterOliver@users.noreply.github.com>
2023-09-19 13:48:23 +0200

    implement MapConfiguration record

--- src/main/java/com/codecool/marsexploration/data/config/MapConfiguration.java
+++ src/main/java/com/codecool/marsexploration/data/config/MapConfiguration.java
@@ -1,4 +1,8 @@
 package com.codecool.marsexploration.data.config;
 
-public record MapConfiguration() {
+public record MapConfiguration(int size, int numberOfMountains, int numberOfPits, int numberOfMinerals,
+                               int numberOfWaters, int numberOfMountainRanges, int numberOfPitCanyons) {
+  public int getNumberOfTiles() {
+    return numberOfMinerals + numberOfMountains + numberOfWaters + numberOfPits;
+  }
 }
