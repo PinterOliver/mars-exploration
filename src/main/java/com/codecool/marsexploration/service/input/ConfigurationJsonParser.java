b6ddd00b01f16bdc9dd52aa33833230b3580e7e8
Oliver Peter Pinter <125395118+PinterOliver@users.noreply.github.com>
2023-09-21 20:34:25 +0200

    add ConfigurationJsonParser interface

---
+++ src/main/java/com/codecool/marsexploration/service/input/ConfigurationJsonParser.java
@@ -0,0 +1,7 @@
+package com.codecool.marsexploration.service.input;
+
+import com.codecool.marsexploration.data.config.MapValidationConfiguration;
+
+public interface ConfigurationJsonParser {
+  MapValidationConfiguration get();
+}
